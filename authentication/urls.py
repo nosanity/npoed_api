cceddbf57b0db83355fd4ade7f2552ed76c0e8f9
nosanity <no-sanity17@ya.ru>
2016-03-31 15:58:34 +0300

    APIBR-18 Сделать ручку /ping для проверки приложениями, что всё ок

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,9 @@
 # coding: utf8
 
 from django.conf.urls import url
+from authentication.views import PingView
 
 urlpatterns = [
     url(r'^register/?$', 'authentication.views.register_app', name='register-app'),
+    url(r'^ping/?$', PingView.as_view(), name='ping'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -2,7 +2,13 @@
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.response import Response
+from rest_framework import status
 from authentication.forms import RegisterAppForm
+from authentication.models import RegisteredApp
 
 
 @login_required
@@ -14,3 +20,21 @@ def register_app(request):
             form.save(commit=True)
             return render(request, 'register.html', context={'form': form, 'instance': form.instance})
     return render(request, 'register.html', context={'form': form})
+
+
+class AppAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        app_id, secret = request.META.get('HTTP_X_ID'), request.META.get('HTTP_X_SECRET')
+        if RegisteredApp.objects.filter(app_id=app_id, secret=secret).exists():
+            return True, True
+        raise AuthenticationFailed()
+
+    def authenticate_header(self, request):
+        return 'Missing or wrong credentials provided'
+
+
+class PingView(APIView):
+    authentication_classes = (AppAuthentication, )
+
+    def get(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_200_OK)
